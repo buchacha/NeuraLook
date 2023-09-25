dceedfc8de8d923d0bf3b1f5b3ce5b66c9aa6792
Rijkaa <51097216+Rijkaa@users.noreply.github.com>
2023-09-25 23:07:40 +0300

    Update process.py

--- process.py
+++ process.py
@@ -1,99 +1,14 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-
 import os
-import time
-import cv2
-import torch
+import io
+import warnings
 import argparse
+import time
 import pathlib
-import numpy as np
+
 from PIL import Image
-from tqdm import tqdm
-
-from torch.utils.data import DataLoader
-import torchvision.transforms as transforms
-
-import networks
-from utils.transforms import transform_logits
-from datasets.simple_extractor_dataset import SimpleFolderDataset
-
-
-def find_new_size(side_ratio):
-    if side_ratio > 1:
-        return (
-            int((1048576 / side_ratio) ** 0.5) // 64 * 64,
-            int(((1048576 / side_ratio) ** 0.5) * side_ratio) // 64 * 64,
-        )
-    else:
-        return (
-            int(((1048576 / side_ratio) ** 0.5) * side_ratio) // 64 * 64,
-            int((1048576 / side_ratio) ** 0.5) // 64 * 64,
-        )
-
-
-def remove_head_from_mask(mask, head_mask):
-    mask_ar = np.array(mask)
-    head_mask_ar = np.array(head_mask)
-    mask_without_head_ar = mask_ar - head_mask_ar
-    mask_without_head_ar = np.where(mask_without_head_ar > 1, 0, 255)
-    mask_without_head_ar = np.asarray(mask_without_head_ar, dtype=np.uint8)
-    mask_without_head = Image.fromarray(mask_without_head_ar, mode="RGB")
-    return mask_without_head
-
-
-def preproc_img_size(img):
-    """
-    Функция для приведения изображения к нужному размеру: либо максимально возможному
-    с исходным соотношением сторон,если исходное изображение содержит больше 2**20 пикселей,
-    либо к размеру, где ширина и высота изображения делятся на 64, если размер изображения
-    не превышает максимально возможный для Stable Diffusion. Возвращает изображение с
-    "правильным" размером
-    Parameters:
-    img - image to resize
-    """
-    min_size_index = np.argmin(img.size)
-    side_ratio = img.size[min_size_index] / img.size[min_size_index - 1]
-    new_size = find_new_size(side_ratio)
-    m = [0, 0]
-    if img.size[min_size_index] > new_size[0]:
-        m[min_size_index] = new_size[0]
-        m[min_size_index - 1] = new_size[1]
-        return img.resize((m))
-    else:
-        m[0] = img.size[0] // 64 * 64
-        m[1] = img.size[1] // 64 * 64
-        return img.resize((m))
-
-
-def erosion(img, iterations, dilation_kernel_size=(5, 5)):
-    """
-    Функция для обратной эрозии маски изображения. Принимает на вход `img` - исходная маска, и
-    `iteration` - количество итераций эрозии, чем больше - тем сильнее эффект
-    """
-    img2 = np.array(img)
-    img2 = img2[:, :, ::-1].copy()
-    kernel = np.ones(dilation_kernel_size, np.uint8)
-    dilation = cv2.erode(img2, kernel, iterations=iterations)
-    dilation = cv2.cvtColor(dilation, cv2.COLOR_BGR2RGB)
-    return Image.fromarray(dilation)
-
-
-def blur(img, blur_kernel_size, sigma):
-    """
-    Функция для размытия маски изображения, чем больше сигма - тем сильнее
-    """
-    blur = transforms.GaussianBlur(kernel_size=blur_kernel_size, sigma=sigma)
-    return blur(img)
-
-
-def dilation(img, iterations):
-    img2 = np.array(img)
-    img2 = img2[:, :, ::-1].copy()
-    kernel = np.ones((5, 5), np.uint8)
-    dilation = cv2.dilate(img2, kernel, iterations=iterations)
-    dilation = cv2.cvtColor(dilation, cv2.COLOR_BGR2RGB)
-    return Image.fromarray(dilation)
+from stability_sdk import client
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+import numpy as np
 
 
 def get_arguments():
@@ -101,148 +16,190 @@ def get_arguments():
     Returns:
       A list of parsed arguments.
     """
-    parser = argparse.ArgumentParser(description="Self Correction for Human Parsing")
+    parser = argparse.ArgumentParser(description="Stable Diffusion")
 
-    parser.add_argument(
-        "--model-restore",
-        type=str,
-        default="",
-        help="restore pretrained model parameters.",
-    )
-    parser.add_argument("--gpu", type=str, default="0", help="choose gpu device.")
     parser.add_argument(
         "--input-dir", type=str, default="", help="path of input image folder."
     )
     parser.add_argument(
-        "--output-dir", type=str, default="", help="path of output image folder."
+        "--mask-dir", type=str, default="", help="path of mask of image folder."
     )
     parser.add_argument(
-        "--container", type=str, default="", help="path to collect processed images"
+        "--cont", type=str, default="", help="path to collect processed images."
+    )
+    parser.add_argument(
+        "--search", type=str, default="", help="path of images using for search."
+    )
+    parser.add_argument(
+        "--output-dir", type=str, default="", help="path of output image folder."
     )
 
     return parser.parse_args()
 
 
-def main():
-    current_path = pathlib.Path(__file__).parent.resolve()
-    args.output_dir = os.path.join(current_path, "output_segmentation")
-    args.cont = os.path.join(current_path, "container_segmentation")
+def setup_env():
+    os.environ["STABILITY_HOST"] = "grpc.stability.ai:443"
+    os.environ["STABILITY_KEY"] = "sk-wJBsRNL0QF0ET6ac08Yim4YjIXBFwP6jPsko5pMmVqxmIQTX"
 
-    opt_blur_kernel_size = 11
-    opt_sigma = 20
-    args = get_arguments()
-
-    gpus = [int(i) for i in args.gpu.split(",")]
-    assert len(gpus) == 1
-    if not args.gpu == "None":
-        os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
-
-    num_classes = 18
-    input_size = [512, 512]
-
-    model = networks.init_model("resnet101", num_classes=num_classes, pretrained=None)
-
-    state_dict = torch.load(args.model_restore)["state_dict"]
-    from collections import OrderedDict
-
-    new_state_dict = OrderedDict()
-
-    for k, v in state_dict.items():
-        name = k[7:]  # remove `module.`
-        new_state_dict[name] = v
-
-    model.load_state_dict(new_state_dict)
-
-    model.cuda()
-    model.eval()
-
-    transform = transforms.Compose(
-        [
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.406, 0.456, 0.485], std=[0.225, 0.224, 0.229]),
-        ]
+    # Set up our connection to the API.
+    stability_api = client.StabilityInference(
+        key=os.environ["STABILITY_KEY"],  # API Key reference.
+        verbose=True,  # Print debug messages.
+        engine="stable-inpainting-v1-0",  # Set the engine to use for generation.
+        # Available engines: stable-diffusion-v1 stable-diffusion-v1-5 stable-diffusion-512-v2-0 stable-diffusion-768-v2-0
+        # stable-diffusion-512-v2-1 stable-diffusion-768-v2-1 stable-diffusion-xl-beta-v2-2-2 stable-inpainting-v1-0 stable-inpainting-512-v2-0
     )
+    return stability_api
 
-    if not os.path.exists(args.output_dir):
-        os.makedirs(args.output_dir)
+
+def create_box(mask):
+    ar = np.array(mask)
+    morphed_white_pixels = np.argwhere(ar == 0)
+
+    min_y = min(morphed_white_pixels[:, 1])
+    max_y = max(morphed_white_pixels[:, 1])
+    min_x = min(morphed_white_pixels[:, 0])
+    max_x = max(morphed_white_pixels[:, 0])
+
+    padding = 20
+    top_left = (min_y - padding, min_x - padding)
+    bottom_right = (max_y + padding, max_x + padding)
+    return top_left, bottom_right
+
+
+def get_promt(image_path):
+    base_prompt = []
+    opt_additional_prompts = "photorealistic, best quality"
+    opt_additional_prompts_weight = 1
+    opt_antiprompts = "face, cartoon, nude, the clothes fit perfectly, tattoo"
+    opt_antiprompts_weight = -1
+    basename = os.path.basename(image_path)
+    list_from_basename = basename[:-4].split(sep="_")
+    prompt_list = list_from_basename[1:-1]
+    prompt = " ".join(prompt_list)
+    base_prompt = [
+        generation.Prompt(
+            text=f"{prompt}, {opt_additional_prompts}",
+            parameters=generation.PromptParameters(
+                weight=opt_additional_prompts_weight
+            ),
+        )
+    ]
+    base_prompt.append(
+        generation.Prompt(
+            text=f"{opt_antiprompts}",
+            parameters=generation.PromptParameters(weight=opt_antiprompts_weight),
+        )
+    )
+    return base_prompt
+
+
+def create_request(api, prompt, image, mask):
+    opt_sh = 0.8  # Степень влияния исходной картинки на начальных шагах (0-1) (меньше значение - больше влияние)
+    opt_end_sh = 0.1  # Степень влияния исходной картинки на конечных шагах (0-1) (меньше значение - больше влияние)
+    opt_cfg = 7  # (1-35) Насколько сильно модель следует промту
+    opt_steps = 30  # Количество шагов генерации (10-150)
+    opt_sampler = generation.SAMPLER_K_EULER_ANCESTRAL
+
+    answers = api.generate(
+        prompt=prompt,
+        init_image=image,
+        mask_image=mask,
+        # mask_source='MASK_IMAGE_BLACK',
+        start_schedule=opt_sh,
+        end_schedule=opt_end_sh,
+        samples=4,
+        # If attempting to transform an image that was previously generated with our API,
+        # initial images benefit from having their own distinct seed rather than using the seed of the original image generation.
+        steps=opt_steps,  # Amount of inference steps performed on image generation. Defaults to 30.
+        cfg_scale=opt_cfg,  # Influences how strongly your generation is guided to match your prompt.
+        # Setting this value higher increases the strength in which it tries to match your prompt.
+        # Defaults to 7.0 if not specified.
+        width=mask.size[0],  # Generation width, defaults to 512 if not included.
+        height=mask.size[1],  # Generation height, defaults to 512 if not included.
+        sampler=opt_sampler  # Choose which sampler we want to denoise our generation with.
+        # Defaults to k_lms if not specified. Clip Guidance only supports ancestral samplers.
+        # (Available Samplers: ddim, plms, k_euler, k_euler_ancestral, k_heun, k_dpm_2, k_dpm_2_ancestral, k_dpmpp_2s_ancestral, k_lms, k_dpmpp_2m, k_dpmpp_sde)
+    )
+    return answers
+
+
+def save_cropped(img, img_name, search_path, top_left, bottom_right):
+    res_im = img.crop((top_left[0], top_left[1], bottom_right[0], bottom_right[1]))
+    res_im.save(os.path.join(search_path, img_name))
+
+
+def save_answer(answers, img_name, save_path, search_path, left_top, right_bottom):
+    i = 0
+    for resp in answers:
+        for artifact in resp.artifacts:
+            if artifact.finish_reason == generation.FILTER:
+                warnings.warn(
+                    "Your request activated the API's safety filters and could not be processed."
+                    "Please modify the prompt and try again."
+                )
+            if artifact.type == generation.ARTIFACT_IMAGE:
+                global img
+                img = Image.open(io.BytesIO(artifact.binary))
+                img.save(os.path.join(save_path, img_name[:-4] + f"_{i}.png"))
+                save_cropped(
+                    img,
+                    img_name[:-4] + f"_{i}.png",
+                    search_path,
+                    left_top,
+                    right_bottom,
+                )
+                i = i + 1
+
+
+def main():
+    args = get_arguments()
+    print("a")
+
+    current_path = pathlib.Path(__file__).parent.resolve()
+    args.input_dir = os.path.join(current_path, "container_segmentation")
+    args.container = os.path.join(current_path, "container_generation")
+    args.mask_dir = os.path.join(current_path, "output_segmentation")
 
     if not os.path.exists(args.container):
         os.makedirs(args.container)
 
-    head_palette = [
-        item for sublist in [[0] * 11 * 3, [255] * 3, [0] * 6 * 3] for item in sublist
-    ]
-    palette = [
-        item
-        for sublist in [[0] * 12, [255] * 3, [0] * 9 * 3, [255] * 6, [0] * 6]
-        for item in sublist
-    ]
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
 
+    if not os.path.exists(args.search):
+        os.makedirs(args.search)
+    print("a")
     while True:
-        if os.listdir(args.input_dir) == []:
+        api = setup_env()
+        list_dir = os.listdir(args.input_dir)
+        print("a")
+        if list_dir == []:
             time.sleep(10)
         else:
-            dataset = SimpleFolderDataset(
-                root=args.input_dir, input_size=input_size, transform=transform
-            )
-            dataloader = DataLoader(dataset)
-            with torch.no_grad():
-                for idx, batch in enumerate(tqdm(dataloader)):
-                    image, meta = batch
-                    img_name = meta["name"][0]
-                    c = meta["center"].numpy()[0]
-                    s = meta["scale"].numpy()[0]
-                    w = meta["width"].numpy()[0]
-                    h = meta["height"].numpy()[0]
-
-                    output = model(image.cuda())
-                    upsample = torch.nn.Upsample(
-                        size=input_size, mode="bilinear", align_corners=True
+            for img_path in list_dir:
+                prompt = get_promt(img_path)
+                image = Image.open(os.path.join(args.input_dir, img_path))
+                mask = Image.open(os.path.join(args.mask_dir, img_path)).convert("L")
+                left_top, right_bottom = create_box(mask)
+                answers = create_request(api, prompt, image, mask)
+                try:
+                    os.rename(
+                        os.path.join(args.input_dir, img_path),
+                        os.path.join(args.cont, img_path),
                     )
-                    upsample_output = upsample(output[0][-1][0].unsqueeze(0))
-                    upsample_output = upsample_output.squeeze()
-                    upsample_output = upsample_output.permute(1, 2, 0)  # CHW -> HWC
-
-                    logits_result = transform_logits(
-                        upsample_output.data.cpu().numpy(),
-                        c,
-                        s,
-                        w,
-                        h,
-                        input_size=input_size,
-                    )
-                    parsing_result = np.argmax(logits_result, axis=2)
-                    parsing_result_path = os.path.join(
-                        args.output_dir, img_name[:-4] + ".png"
-                    )
-                    save_to_container_path = os.path.join(
-                        args.cont, img_name[:-4] + ".png"
-                    )
-                    cont_image = preproc_img_size(
-                        Image.open(os.path.join(args.input_dir, img_name))
-                    )
-                    cont_image.save(save_to_container_path)
-                    os.remove(os.path.join(args.input_dir, img_name))
-                    output_img = Image.fromarray(
-                        np.asarray(parsing_result, dtype=np.uint8)
-                    )
-                    head_img = output_img.copy()
-                    head_img.putpalette(head_palette)
-                    output_img.putpalette(palette)
-                    dil_param = img_name[:-4].split(sep="_")[-1]
-                    output_img = dilation(output_img.convert("RGB"), int(dil_param))
-                    output_img = remove_head_from_mask(
-                        output_img.convert("RGB"), head_img.convert("RGB")
-                    )
-                    output_img = blur(
-                        img=output_img,
-                        blur_kernel_size=opt_blur_kernel_size,
-                        sigma=opt_sigma,
-                    )
-                    output_img = preproc_img_size(output_img)
-                    output_img.save(parsing_result_path)
-                    if img_name[:-4] == "stop_the_process":
-                        return
+                except FileExistsError:
+                    os.remove(os.path.join(args.input_dir, img_path))
+                save_answer(
+                    answers,
+                    img_path,
+                    args.output_dir,
+                    args.search,
+                    left_top,
+                    right_bottom,
+                )
+                if img_path[:-4] == "stop_the_process":
+                    return
 
 
 if __name__ == "__main__":
